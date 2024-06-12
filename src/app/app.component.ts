5fd16bded58f30920f38f42616b678738ffeb2d8
Pooria Eshghi <https://github.com/PooriaEshghi>
2024-06-12 15:14:59 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'test';
-}
+export class AppComponent {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+selector: 'app-header',
+templateUrl:'./header.component.html',
+standalone:true,
+styleUrl:'header.component.css'
+})
+export class HeaderComponent{}
